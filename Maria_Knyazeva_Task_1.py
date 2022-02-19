81cc810335ea336a540987ce14a5e4cb1dfadb9e
knmary <97901017+knmary@users.noreply.github.com>
2022-02-19 21:10:11 +0400

    Delete Maria_Knyazeva_Task_1.py

--- Maria_Knyazeva_Task_1.py
+++
@@ -1,9 +0,0 @@
-DICT_MAIN = {'zero':'ноль','one':'один','two':'два','three':'три','four':'четыре','five':'пять','six':'шесть','seven':'семь','eight':'восемь','nine':'девять','ten':'десять'}
-word_main = str(input('Введите числительное от 0 до 10 на английском языке:'))
-
-def num_translate(word):
-    for word in DICT_MAIN.keys():
-        word = DICT_MAIN.get(word_main, None)
-    return word
-
-print(num_translate(word_main))
